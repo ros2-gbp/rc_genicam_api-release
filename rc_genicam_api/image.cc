cc1ec38dadd8d18ef78c03f2fb02136a83a6851b
Felix Ruess <felix.ruess@roboception.de>
2024-03-12 15:56:37 +0100

    Rebase from 'debian/humble/rc_genicam_api'

--- rc_genicam_api/image.cc
+++ rc_genicam_api/image.cc
@@ -40,10 +40,15 @@
 
 #include <cstring>
 
+#ifdef _WIN32
+#undef min
+#undef max
+#endif
+
 namespace rcg
 {
 
-Image::Image(const Buffer *buffer, std::uint32_t part)
+Image::Image(const Buffer *buffer, uint32_t part)
 {
   if (buffer->getImagePresent(part))
   {
@@ -59,7 +64,7 @@ Image::Image(const Buffer *buffer, std::uint32_t part)
     pixelformat=buffer->getPixelFormat(part);
     bigendian=buffer->isBigEndian();
 
-    const size_t size=buffer->getSize(part);
+    const size_t size=std::min(buffer->getSize(part), buffer->getSizeFilled());
 
     pixel.reset(new uint8_t [size]);
 
